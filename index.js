67dd43396d1970d8369a929a68c0f4970c24a464
jad <jadesrochers@gmail.com>
2019-10-29 12:06:21 -0400

    remove the zoombutton export

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
 export { GeoSvg } from './src/geosvg'
 export { ToolTipSvg } from './src/svgtools'
 export { UsState, UsCounty, UsCountyOnly } from './src/usmaps'
-export { ZoomButtons } from './src/zoombutton'
-
