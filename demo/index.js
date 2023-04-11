0666a845a3b5ff6fa5e852fd7099445b243b96ab
jadesrochers <jadesrochers@gmail.com>
2023-04-10 22:14:50 -0400

    update demo index and webpack

--- demo/index.js
+++ demo/index.js
@@ -1,8 +1,10 @@
-import * as ReactDOMClient from "react-dom/client";
+// Have not found a way to avoid importing React
+import React from "react";
+import { createRoot } from "react-dom/client";
 import { App } from "./component/App.js";
 
-const container = document.getElementById('app');
-
-const root = ReactDOMClient.createRoot(container)
+// This is the element ID from you index.html you want the app to use
+const container = document.getElementById('root');
+const root = createRoot(container)
 
 root.render(<App />);
