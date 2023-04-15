8d658fab829c11a7efcff252c2836d4afb7fa769
jadesrochers <jadesrochers@gmail.com>
2023-04-15 15:46:22 -0400

    Update demo with react-router

--- demo/component/App.js
+++ demo/component/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, Router } from '@reach/router';
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom"
 import { StateMap } from "./StateMap";
 import { WorldMap } from "./WorldMap";
 import { CountyMap } from "./CountyMap";
@@ -7,43 +7,45 @@ import { CountyMap } from "./CountyMap";
 import './basic.css'
 
 const NotFound = () => {
-  return(
-    <div style={{ color: '#151515' }} >
-      <h1> 404; Did not find that page, <br/> perhaps try another? </h1>
-    </div>
-  )
+    return(
+        <div style={{ color: '#151515' }} >
+        <h1> 404; Did not find that page, <br/> perhaps try another? </h1>
+        </div>
+    )
 }
 
 const DemoLink = (props) => (
     <Link to={props.link} style={{ margin: '10px' }} >
-      <div style={{ borderRadius: '25px/20px', backgroundColor: 'hsla(200,65%,50%,01)', padding: '5px 15px' }} >
-         { props.display }
-      </div>
+    <div style={{ borderRadius: '25px/20px', backgroundColor: 'hsla(200,65%,50%,01)', padding: '5px 15px' }} >
+    { props.display }
+    </div>
     </Link>
 )
 
 const Home = (props) => {
- return (
-  <div style={{ display: "flex", flexDirection: "column", alignItems: "center", width: '100%' }} >
-   <DemoLink link="statemap" display="US State map demo" />
-   <DemoLink link="worldmap" display="World Map demo" />
-   <DemoLink link="countymap" display="US Full State/County Map demo" />
-  </div>
- )
+    return (
+        <div style={{ display: "flex", flexDirection: "column", alignItems: "center", width: '100%' }} >
+        <DemoLink link="statemap" display="US State map demo" />
+        <DemoLink link="worldmap" display="World Map demo" />
+        <DemoLink link="countymap" display="US Full State/County Map demo" />
+        </div>
+    )
 }
 
 function App() {
-  return (
-    <div>
-      <Router width='100%'  >
-        <StateMap path="/statemap" />
-        <WorldMap path="/worldmap" />
-        <CountyMap path="/countymap" />
-        <NotFound default />
-        <Home path="/"/>
-      </Router>
-    </div>
-  );
+    return (
+        <div>
+        <BrowserRouter width='100%'  >
+            <Routes>
+                <Route  path="/statemap" element={<StateMap />} />
+                <Route  path="/worldmap" element={<WorldMap />} />
+                <Route  path="/countymap" element={<CountyMap />} />
+                <Route  default element={<NotFound />} />
+                <Route  path="/" element={<Home />} />
+            </Routes>
+        </BrowserRouter>
+        </div>
+    );
 }
 
 export { App }
