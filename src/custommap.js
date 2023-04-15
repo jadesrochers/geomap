95eea6d893fc2c897918ee8761d0d3d4fc6ecdf3
jadesrochers <jadesrochers@gmail.com>
2023-04-15 15:47:26 -0400

    Formatting of some files

--- src/custommap.js
+++ src/custommap.js
@@ -10,7 +10,7 @@ import { scaleQuantile } from 'd3-scale';
 
 const GnYlRd73 = [  '#005a32', '#238443', '#41ab5d', '#78c679', '#addd8e', '#d9f0a3', '#ffffcc', '#ffeda0', '#feb24c', '#f03b20']
 const projectAlbersUsa = R.curry( (scale, xtrans, ytrans) =>
-  geoAlbersUsa()
+    geoAlbersUsa()
     .scale(scale)  
     .translate([xtrans, ytrans]) );
 
@@ -19,45 +19,45 @@ const quantile = R.curry((outputRange, data) => scaleQuantile().domain(R.values(
 const defaultHighlight = {'stroke-width':2, fill:'#5d6d7e'}
 
 const CustomMap = (props) => {
-  return(
-   <BaseMap 
-      projection={ props.projection ? props.projection : projectAlbersUsa }
-      scaling={props.scaling ? props.scaling : 1000}
-      { ...props }
-   >
-    <MouseRect key="mousecapture" height="99%" width="99%" />
-    <SvgGeneric 
-      key="genericmap1"
-      style={props.style} 
-      datastyle={props.datastyle} 
-    />
-   </BaseMap>
-  )
+    return(
+        <BaseMap 
+        projection={ props.projection ? props.projection : projectAlbersUsa }
+        scaling={props.scaling ? props.scaling : 1000}
+        { ...props }
+        >
+        <MouseRect key="mousecapture" height="99%" width="99%" />
+        <SvgGeneric 
+        key="genericmap1"
+        style={props.style} 
+        datastyle={props.datastyle} 
+        />
+        </BaseMap>
+    )
 }
 
 const SvgGeneric = (props) => {
-  // Since the feature type is unknown, need a featurename argument
-  const geofeatures = useLoadgeo(props.getgeofeat, props.featurename)
-  const [highlight, deHighlight] = createHighlight()
-  const pass = R.dissoc('style')(props)
+    // Since the feature type is unknown, need a featurename argument
+    const geofeatures = useLoadgeo(props.getgeofeat, props.featurename)
+    const [highlight, deHighlight] = createHighlight()
+    const pass = R.dissoc('style')(props)
 
-  if( ! geofeatures ){
-    return null
-  }
-  /* console.log('geocounty object value:\n',geocounty) */
-  return(
-    <GeoSvg 
-      key={'custom_geosvg'}
-      topology={ geofeatures }
-      topopath={props.featurename}
-      colorize={ props.colorize ? props.colorize : quantile(GnYlRd73) }
-      style={props.style}
-      datastyle={props.datastyle}
-      highlight={highlight(props.highlightStyle ? props.highlightStyle : defaultHighlight)}
-      deHighlight={deHighlight}
-      { ...pass }
-    />
-  )
+    if( ! geofeatures ){
+        return null
+    }
+    /* console.log('geocounty object value:\n',geocounty) */
+    return(
+        <GeoSvg 
+        key={'custom_geosvg'}
+        topology={ geofeatures }
+        topopath={props.featurename}
+        colorize={ props.colorize ? props.colorize : quantile(GnYlRd73) }
+        style={props.style}
+        datastyle={props.datastyle}
+        highlight={highlight(props.highlightStyle ? props.highlightStyle : defaultHighlight)}
+        deHighlight={deHighlight}
+        { ...pass }
+        />
+    )
 }
 
 
--- src/svgtools.js
+++ src/svgtools.js
@@ -4,25 +4,25 @@ import * as R from 'ramda';
 import { jsx } from "@emotion/react";
 
 const getXY = (viewarr, bounds, width, height, scale) => {
-  let x = (bounds[1][0] + bounds[0][0])/2 - width/2
-  let y = (bounds[0][1]) - (height + 10)
-  let scaleInv = 1/scale
-  if(y < viewarr[1]){
-    y = bounds[1][1] + 20
-  }
-  if(x + width > viewarr[2]){
-    x = bounds[0][0] - width - 20
-    y = (bounds[1][1] + bounds[0][1])/2 - height/2
-  }
-  if(x + width < viewarr[0]){
-    x = bounds[1][0] + 20
-    y = (bounds[1][1] + bounds[0][1])/2 - height/2
-  }
-  // Seems like this should not work, (width for x, height for y)
-  // but it does work.
-  x = x  + (height - height*scale)
-  y = y  + (height - height*scale) 
-  return {x, y}
+    let x = (bounds[1][0] + bounds[0][0])/2 - width/2
+    let y = (bounds[0][1]) - (height + 10)
+    let scaleInv = 1/scale
+    if(y < viewarr[1]){
+        y = bounds[1][1] + 20
+    }
+    if(x + width > viewarr[2]){
+        x = bounds[0][0] - width - 20
+        y = (bounds[1][1] + bounds[0][1])/2 - height/2
+    }
+    if(x + width < viewarr[0]){
+        x = bounds[1][0] + 20
+        y = (bounds[1][1] + bounds[0][1])/2 - height/2
+    }
+    // Seems like this should not work, (width for x, height for y)
+    // but it does work.
+    x = x  + (height - height*scale)
+    y = y  + (height - height*scale) 
+    return {x, y}
 }
 // It is a tooltip that uses a <rect> and two <text> elements
 // to pop up a display for any shape rendered.
@@ -32,39 +32,39 @@ const getXY = (viewarr, bounds, width, height, scale) => {
 // These are tooltiprectstyle and tooltipstyle
 // props.scale is used to scale the tooltip if there is zoom in/out.
 const ToolTipSvg = (props) => {
-  if(! props.tooltip){return null} 
-  const bounds = props.tooltip.bounds
-  const viewarr = R.map(parseInt, R.split(' ',props.viewBox))
+    if(! props.tooltip){return null} 
+    const bounds = props.tooltip.bounds
+    const viewarr = R.map(parseInt, R.split(' ',props.viewBox))
 
-  const data = (R.isEmpty(props.tooltip.data) || R.isNil(props.tooltip.data)) ? 'No Value' : (props.tooltip_round ? props.tooltip_round(props.tooltip.data) : Math.round(props.tooltip.data))
-  const featprops = props.tooltip.feature.properties
-  /* console.log('Foreignobject featprops: ', featprops) */
-  /* console.log('Foreignobject props: ', props) */
-  const defaultname = featprops.NAME ? 'NAME' : 'name'
-  const textstyle = {display: 'flex', justifyContent: 'center', alignItems: 'center', color: 'black', fontSize: '2.0rem', height: props.height, width: props.width}
-  const name = props.tooltipkey ? props.tooltipkey : defaultname
-  const toolstyle = (props.tooltipstyle ? props.tooltipstyle : undefined) 
-  const scale = props.scale ? 1/props.scale : 1
+    const data = (R.isEmpty(props.tooltip.data) || R.isNil(props.tooltip.data)) ? 'No Value' : (props.tooltip_round ? props.tooltip_round(props.tooltip.data) : Math.round(props.tooltip.data))
+    const featprops = props.tooltip.feature.properties
+    /* console.log('Foreignobject featprops: ', featprops) */
+    /* console.log('Foreignobject props: ', props) */
+    const defaultname = featprops.NAME ? 'NAME' : 'name'
+    const textstyle = {display: 'flex', justifyContent: 'center', alignItems: 'center', color: 'black', fontSize: '2.0rem', height: props.height, width: props.width}
+    const name = props.tooltipkey ? props.tooltipkey : defaultname
+    const toolstyle = (props.tooltipstyle ? props.tooltipstyle : undefined) 
+    const scale = props.scale ? 1/props.scale : 1
 
-  const { x, y } = getXY(viewarr, bounds, props.width, props.height, scale)
+    const { x, y } = getXY(viewarr, bounds, props.width, props.height, scale)
 
-  const defaultstyle = {fill: '#b0b0b0', fillOpacity: 0.7}
-  return(
-   <svg x={x} y={y} width={props.width} height={props.height} id='tooltipwhole'  >
-     <rect transform={`scale(${scale})`}
-      css={(props.tooltiprectstyle ? props.tooltiprectstyle : defaultstyle)}
-      width={props.width} height={props.height}
-      />
-     <text x={'50%'} y={'35%'} dominantBaseline={'middle'} textAnchor={'middle'} css={[ textstyle, toolstyle ]} transform={`scale(${scale})`}
->
-       {featprops[name]}
-     </text>
-     <text x={'50%'} y={'70%'} dominantBaseline={'middle'} textAnchor={'middle'} css={[ textstyle, toolstyle ]} transform={`scale(${scale})`}
->
-       Data: {data}
-     </text>
-   </svg>
-  )
+    const defaultstyle = {fill: '#b0b0b0', fillOpacity: 0.7}
+    return(
+        <svg x={x} y={y} width={props.width} height={props.height} id='tooltipwhole'  >
+        <rect transform={`scale(${scale})`}
+        css={(props.tooltiprectstyle ? props.tooltiprectstyle : defaultstyle)}
+        width={props.width} height={props.height}
+        />
+        <text x={'50%'} y={'35%'} dominantBaseline={'middle'} textAnchor={'middle'} css={[ textstyle, toolstyle ]} transform={`scale(${scale})`}
+        >
+        {featprops[name]}
+        </text>
+        <text x={'50%'} y={'70%'} dominantBaseline={'middle'} textAnchor={'middle'} css={[ textstyle, toolstyle ]} transform={`scale(${scale})`}
+        >
+        Data: {data}
+        </text>
+        </svg>
+    )
 }
 
 export { ToolTipSvg }
